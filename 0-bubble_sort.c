f4203eb29ae43bd18880d8684a6873362b164264
Richard <rclancing@gmail.com>
2023-04-15 04:33:39 -0700

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using bb sort algorithm
+ * @array: array to be sorted
+ * @size:  size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int temp;
+	bool swap;
+       
+
+	while (1)
+	{
+		swap = false;
+		i = 0;
+		while (i < size -1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swap = true;
+				print_array(array, size);
+			}
+			i++;
+		}
+		if (swap == false)
+			break;
+	}
+}
+
