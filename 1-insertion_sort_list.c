ed766c57de01008edd95db90e574403426561950
Richard <rclancing@gmail.com>
2023-04-15 05:56:02 -0700

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort a doubly linked list in asc using insert
+ * @list: doubly linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp, *node;
+
+	node = *list;
+
+	if (!node)
+		return;
+	while (node)
+	{
+		while (node->next && node->n > node->next->n)
+		{
+			tmp = node->next;
+			node->next = tmp->next;
+
+			if (tmp->next != NULL)
+				tmp->next->prev = node;
+			tmp->next = node;
+
+			if (node->prev != NULL)
+				node->prev->next = tmp;
+			tmp->prev = node->prev;
+			node->prev = tmp;
+
+			if (tmp->prev == NULL)
+				*list = tmp;
+			else
+				node = tmp->prev;
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+
